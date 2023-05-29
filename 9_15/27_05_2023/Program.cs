f4e93b2c77019738cd9dcab72394126a2b1344b1
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-05-29 13:12:35 -0300

    f
    f

--- 9_15/27_05_2023/Program.cs
+++ 9_15/27_05_2023/Program.cs
@@ -1,7 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Declaração das variáveis 'numero1' e 'numero2' e atribuição dos valores 5 a ambas
 int numero1 = 5;
 int numero2 = 5;
 
+// Adição dos valores das variáveis 'numero1' e 'numero2' e armazenamento do resultado na variável 'resultado'
 int resultado = numero1 + numero2;
 
-Console.WriteLine("Resultado da soma é "+resultado);
+// Impressão do resultado da soma no console
+Console.WriteLine("Resultado da soma é " + resultado);

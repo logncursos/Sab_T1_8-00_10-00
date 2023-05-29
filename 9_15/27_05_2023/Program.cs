ff5f519e9c8373140964e93edf78e6133c60d0d1
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-05-29 13:46:59 -0300

    f
    f

--- 9_15/27_05_2023/Program.cs
+++ 9_15/27_05_2023/Program.cs
@@ -1,9 +1,16 @@
-﻿// Declaração das variáveis 'numero1' e 'numero2' e atribuição dos valores 5 a ambas
+﻿// Definição dos números: Dois números, numero1 e numero2, são escolhidos e atribuídos o valor 5 cada um.
 int numero1 = 5;
 int numero2 = 5;
 
-// Adição dos valores das variáveis 'numero1' e 'numero2' e armazenamento do resultado na variável 'resultado'
+// Soma dos números: Os valores de numero1 e numero2 são adicionados e o resultado é guardado na variável resultado.
 int resultado = numero1 + numero2;
 
-// Impressão do resultado da soma no console
-Console.WriteLine("Resultado da soma é " + resultado);
+// Criação da mensagem: A parte fixa da mensagem "Resultado da soma é " é combinada com o valor de resultado usando o operador de adição (+).
+string mensagem = "Resultado da soma é " + resultado;
+
+// Impressão do resultado: A mensagem completa é exibida no console para mostrar o valor da soma.
+Console.WriteLine(mensagem); // O valor da soma será exibido no console.
+
+
+//Resumidamente, o código recebe dois números, soma esses números e mostra o resultado no console. 
+//Utiliza-se a concatenação para juntar a mensagem fixa com o valor da soma e criar a mensagem completa.

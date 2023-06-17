f9725153ec7800b4037c0da6f1e075388b9ad9f6
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-06-17 12:22:38 -0300

    Codigo das coisas

--- Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 4 - 10_06_2023/Program.cs
+++ Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 4 - 10_06_2023/Program.cs
@@ -1,5 +1,8 @@
-﻿int tabuadaDo = 5;
+﻿int tabuadaDo = 5;  // Declara e inicializa a variável "tabuadaDo" com o valor 5.
 
-for(int i = 0; i <= 10; i++){
+// Loop que itera de 0 a 10
+for (int i = 0; i <= 10; i++)
+{
+    // Imprime a tabuada do número "tabuadaDo" multiplicado pelo número atual "i"
     Console.WriteLine(tabuadaDo + " x " + i + " = " + i * tabuadaDo);
 }

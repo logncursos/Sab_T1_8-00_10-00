ca7e19728bccf5b93a2bdae656bb65d43126fd69
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-06-17 12:20:53 -0300

    Codigo das coisas

--- Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 5 - 17_06_2023/Program.cs
+++ Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 5 - 17_06_2023/Program.cs
@@ -1,9 +1,32 @@
-﻿for(int i = 0; i < 5; i++){
-    
-    if(i % 2 == 0){
+﻿// Exercicio Par ou Impar
+Console.WriteLine("---------Par ou Impar---------");
+
+// Loop que itera de 0 a 4
+for (int i = 0; i < 5; i++)
+{
+    // Verifica se o número é par
+    if (i % 2 == 0)
+    {
+        // Imprime o número e a palavra "Par"
         Console.WriteLine(i + " Par");
-    } else {
+    }
+    else
+    {
+        // Imprime o número e a palavra "Impar"
         Console.WriteLine(i + " Impar");
     }
-
 }
+
+// Exercicio Nome e Sobrenome
+Console.WriteLine("---------Digitar nome e sobrenome---------");
+
+// Solicita ao usuário que digite o nome
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+// Solicita ao usuário que digite o sobrenome
+Console.WriteLine("Digite seu sobrenome: ");
+string sobrenome = Console.ReadLine();
+
+// Imprime o nome completo concatenando o nome e o sobrenome
+Console.WriteLine("Nome completo: " + nome + " " + sobrenome);

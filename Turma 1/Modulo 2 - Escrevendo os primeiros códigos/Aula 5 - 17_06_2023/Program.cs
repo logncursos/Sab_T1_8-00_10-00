7b6cd09cde0bcee5cae315d87fd2efb13029cdf5
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-06-17 12:16:24 -0300

    Codigo das coisas

--- Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 5 - 17_06_2023/Program.cs
+++ Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 5 - 17_06_2023/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for(int i = 0; i < 5; i++){
+    
+    if(i % 2 == 0){
+        Console.WriteLine(i + " Par");
+    } else {
+        Console.WriteLine(i + " Impar");
+    }
+
+}

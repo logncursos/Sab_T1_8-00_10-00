2e230879b4343ef30f1d5ffea4ef64246784b46c
BrunoBafilli <brunobafilli@gmail.com>
2023-07-08 15:28:27 -0300

    Codigo das coisas
    Codigo das coisas

--- Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 8 - 08_07_2023/Program.cs
+++ Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 8 - 08_07_2023/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for(int i = 0; i <= 100; i++){
+    Console.WriteLine(i + " - " + (char)i);
+}

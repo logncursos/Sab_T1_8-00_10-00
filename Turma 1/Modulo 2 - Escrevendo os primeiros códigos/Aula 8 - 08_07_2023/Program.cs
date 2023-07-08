b3c0056bd861623699873c84cf2975fb98955168
BrunoBafilli <brunobafilli@gmail.com>
2023-07-08 15:34:17 -0300

    Codigo das coisas
    Codigo das coisas

--- Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 8 - 08_07_2023/Program.cs
+++ Turma 1/Modulo 2 - Escrevendo os primeiros códigos/Aula 8 - 08_07_2023/Program.cs
@@ -1,3 +1,11 @@
-﻿for(int i = 0; i <= 100; i++){
+﻿//Atividade 1
+for(int i = 0; i <= 100; i++){
     Console.WriteLine(i + " - " + (char)i);
 }
+
+//atividade 2
+const double PI = 3.14;
+Console.WriteLine("Digite um numero");
+double digitado = Convert.ToInt32(Console.ReadLine());
+double resultado = PI * digitado;
+Console.WriteLine(resultado);

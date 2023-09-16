572839be5a39efc6fccf024fa1ed5a348f407966
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-09-16 11:29:20 -0300

    LogN
    LogN

---
+++ Códigos/Modulo 4 -/if ternario/Program.cs
@@ -0,0 +1,46 @@
+﻿// string numeros = "-5 3 8 10 12 -2 7";
+
+int[] numeros = new int[7] {-5, 3, 8, 10, 12, -2, 7};
+
+int qntNumeros = numeros.Length;
+
+int maiorNumero = 0;
+int menorNumero = 0;
+int qntNegativos = 0;
+int qntPositivos = 0;
+
+double total = 0;
+
+for(int i = 0; i < numeros.Length; i++){
+    total += numeros[i];
+
+    maiorNumero = maiorNumero < numeros[i] ? numeros[i] : maiorNumero;
+
+    // if(maiorNumero < numeros[i])
+    //     maiorNumero = numeros[i];
+
+    menorNumero = menorNumero > numeros[i] ? numeros[i] : menorNumero;
+
+    // if(menorNumero > numeros[i])
+    //     menorNumero = numeros[i];
+
+    int teste = numeros[i] < 0 ? qntNegativos++ : qntPositivos++;
+
+    // if(numeros[i] < 0)
+    //     qntNegativos++;
+    // else
+    //     qntPositivos++;
+}
+
+
+Console.WriteLine($"Media {total / qntNumeros}");
+Console.WriteLine($"Maior numero {maiorNumero}");
+Console.WriteLine($"Menor numero {menorNumero}");
+Console.WriteLine($"quantidade positivos {qntPositivos}");
+Console.WriteLine($"quantidade negativos {qntNegativos}");
+
+// Média: 4.71
+// Maior número: 12
+// Menor número: -5
+// Números positivos: 5
+// Números negativos: 2

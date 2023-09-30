49c58dd00f4f955869d81d5363452f2438eb5de7
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-09-30 10:21:31 -0300

    logn
    logn

---
+++ Códigos/Modulo 4 -/while/Program.cs
@@ -0,0 +1,16 @@
+﻿// while (false)
+// {
+//    Console.WriteLine("while"); 
+// }
+
+// bool mercado = true;
+// do
+// {
+
+//     Console.WriteLine("Estado do mercado " + mercado);
+
+// } while (mercado);
+
+
+//Pedir ao usuário para digitar números até que 
+//um número negativo seja digitado utilizando um loop while

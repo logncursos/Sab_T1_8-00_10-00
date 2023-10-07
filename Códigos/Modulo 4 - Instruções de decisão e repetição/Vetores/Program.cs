4427dd3cab97aab60f785b9549820383626a79fb
BrunoBafilli <brunobafilli@gmail.com>
2023-10-07 15:43:26 -0300

    Vetores
    Vetores

---
+++ Códigos/Modulo 4 - Instruções de decisão e repetição/Vetores/Program.cs
@@ -0,0 +1,17 @@
+﻿// Criando um vetor (array) de inteiros
+int[] vetor = new int[5];
+
+// Inicializando o vetor com valores
+vetor[0] = 1;
+vetor[1] = 2;
+vetor[2] = 3;
+vetor[3] = 4;
+vetor[4] = 5;
+
+// Acessando elementos do vetor pelo índice
+int primeiroElemento = vetor[0];  // O primeiro elemento (índice 0) é 1
+int segundoElemento = vetor[1];   // O segundo elemento (índice 1) é 2
+
+// Imprimindo os valores do vetor
+Console.WriteLine(primeiroElemento);
+Console.WriteLine(segundoElemento);

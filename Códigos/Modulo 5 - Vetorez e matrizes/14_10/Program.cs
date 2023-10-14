247c73023d6686ea8defb646929f0e500c28be3d
BrunoBafilli <brunobafilli@gmail.com>
2023-10-14 12:29:54 -0300

    Vetores

---
+++ Códigos/Modulo 5 - Vetorez e matrizes/14_10/Program.cs
@@ -0,0 +1,24 @@
+﻿// int[] marcio = new int[5] { 5, 8, 2, 7, 1 };
+// int media = 0;
+// int notaMinima = 6;
+//
+// for (int i = 0; i < marcio.Length; i++) 
+//     media += marcio[i];
+//
+// media /= marcio.Length;
+//
+// bool aprovado = media >= notaMinima ? true : false;
+//
+// Console.WriteLine($"{aprovado} - {media}");
+
+// string[] produto = new string[3] { "mouse", "teclado", "monitor" };
+// double[] preco = new double[3] { 6.30, 35.3, 5.60 };
+// double total = 0;
+//
+// for (int i = 0; i < produto.Length; i++)
+// {
+//     Console.WriteLine($"{produto[i]} -> {preco[i]}");
+//     total += preco[i];
+// }
+//
+// Console.WriteLine(total.ToString(format:"C"));

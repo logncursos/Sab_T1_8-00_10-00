dbf7db8a87760a2c75e45aaa458cb4855c871dc3
BrunoBafilli <134939811+logncursos@users.noreply.github.com>
2023-10-29 10:24:23 -0300

    t
    t

--- Códigos/Modulo 5 - Vetorez e matrizes/28_10/Program.cs
+++ Códigos/Modulo 5 - Vetorez e matrizes/28_10/Program.cs
@@ -1,27 +1,24 @@
-﻿int[,] multiDimensionalArray1 = new int[2, 3];
-//1,2,- 3
-//3,4,- 7
-
-multiDimensionalArray1[0,0] = 1;
-multiDimensionalArray1[0,1] = 2;
-multiDimensionalArray1[1,0] = 3;
-multiDimensionalArray1[1,1] = 4;
+﻿int[,] multiDimensionalArray1 = new int[2, 4] { { 1, 2, 5, 0 }, { 3, 4, 6, 0 } };
 
 for (int l = 0; l < 2; l++)
 {
     int total = 0;
-    for(int c = 0; c < 3; c++){
-
-        total += multiDimensionalArray1[l,c];
-
-        if(c == 2){
-            multiDimensionalArray1[l,c] += total;
-        }
-        Console.Write(multiDimensionalArray1[l,c]);
+    for (int c = 0; c < 3; c++)
+    {
+        total += multiDimensionalArray1[l, c];
+        multiDimensionalArray1[l, 3] = total;
+        Console.Write(multiDimensionalArray1[l, c] + " ");
     }
     Console.WriteLine();
 }
+
 Console.WriteLine("------");
 
-Console.WriteLine(multiDimensionalArray1[0,2]);
-Console.WriteLine(multiDimensionalArray1[1,2]);
+for (int l = 0; l < 2; l++)
+{
+    for (int c = 0; c < 4; c++)
+    {
+        Console.Write(multiDimensionalArray1[l, c] + " ");
+    }
+    Console.WriteLine();
+}
